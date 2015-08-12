15d81994d9c5ded601060a44a3af7246e1890212
Felix Schlitter <felixschlitter@gmail.com>
2015-08-12 16:43:00 +1200

    Fix indentation

--- src/run-hook.js
+++ src/run-hook.js
@@ -46,53 +46,54 @@ temp.track();
 export default function run(hook, args) {
   return Promise.resolve(co(function*() {
     const root = path.resolve(yield git.getGitRepoRoot(), '..');
+
+    /**
+     * Load YAML configuration file
+     */
+
     const config = yaml.safeLoad(
       yield fs.readFileAsync(
         path.resolve(root, '.githooks.yml')));
 
-      /**
-       * Assemble the script to run
-       */
+    /**
+     * Assemble the script to run
+     */
 
-      debug('Assembling script...');
-      const header =
-      [ '#!/usr/bin/env bash'
-      , 'set -eo pipefail'
-      ].join('\n');
-      const script = _.foldl(config[hook], (acc, cmd) =>
-        (acc + [ ``
-               , `echo $ ${ shellescape([chalk.green(cmd)]) }`
-               , cmd
-               , ``
-               ].join('\n'))
-      , header);
-      debug(script);
+    debug('Assembling script...');
+    const header =
+    [ '#!/usr/bin/env bash'
+    , 'set -eo pipefail'
+    ].join('\n');
+    const script = _.foldl(config[hook], (acc, cmd) =>
+      (acc + [ ``
+             , `echo $ ${ shellescape([chalk.green(cmd)]) }`
+             , cmd
+             , ``
+             ].join('\n'))
+    , header);
+    debug(script);
 
-      /**
-       * Create a temporary file to execute
-       */
+    /**
+     * Create a temporary file to execute
+     */
 
-      debug('Creating temporary file...');
-      const info = yield temp.openAsync('git-hooks');
-      debug(info.path);
-      yield fs.writeFileAsync(info.path, script);
-      yield fs.chmodAsync(info.path, '755');
+    debug('Creating temporary file...');
+    const info = yield temp.openAsync('git-hooks');
+    debug(info.path);
+    yield fs.writeFileAsync(info.path, script);
+    yield fs.chmodAsync(info.path, '755');
 
-      /**
-       * Execute the script
-       */
+    /**
+     * Execute the script
+     */
 
-      debug('Running script...');
-      yield new Promise((resolve, reject) => {
-        const child = spawn('bash', ['--login', info.path]);
-        child.stderr.on('data', data => console.error(data.toString('utf-8')));
-        child.stdout.on('data', data => console.log(data.toString('utf-8')));
-        child.on('error', reject);
-        child.on('close', resolve);
-      })
-  }))
-    .catch(e => {
-      console.log(e);
-      throw e;
-    });
+    debug('Running script...');
+    yield new Promise((resolve, reject) => {
+      const child = spawn('bash', ['--login', info.path]);
+      child.stderr.on('data', data => console.error(data.toString('utf-8')));
+      child.stdout.on('data', data => console.log(data.toString('utf-8')));
+      child.on('error', reject);
+      child.on('close', resolve);
+    })
+  }));
 }
